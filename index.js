0d4212694bd70ff3349ac203990e983e09fdf288
Tommaso <55707296+tompedro@users.noreply.github.com>
2021-03-24 00:04:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ image.addEventListener('click', () => {
   if (audio.paused) {
     audio.play()
     //setTimeout(flip, 1000);
-    image.src = "./res/full.png"
+    image.src = "./res/full.PNG"
     vodka.style.visibility = "visible"
   } else {
     audio.pause()
@@ -21,9 +21,9 @@ window.onbeforeunload = () => {
 function flip(){
     console.log(image.src);
     if(image.src == "file:///C:/Users/Nox/Desktop/russia/res/full2.png"){
-        image.src = "./res/full.png"
+        image.src = "./res/full.PNG"
     }else{
-        image.src = "./res/full2.png"
+        image.src = "./res/full2.PNG"
     }
     setTimeout(flip, 500);
 }
